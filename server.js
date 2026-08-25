9e84032eb3a4be9e03adf079c84346a6f55bbbdc
mohammeddheyaa00-bit <mohammeddheyaa00@gmail.com>
2026-08-25 23:34:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
+const express = require('express');
+const app = express();
+
 const VERIFY_TOKEN = "Samsung10mohammed.";
 
-// دالة التحقق المشتركة
-const handleVerification = (req, res) => {
+app.get('/', (req, res) => {
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
-            return res.status(200).send(challenge);
-        } else {
-            return res.sendStatus(403);
-        }
+    if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
+        return res.status(200).send(challenge);
     }
     return res.status(200).send("Nixe Server is running!");
-};
+});
 
-// يغطي المسار الأساسي ومسار الويب هوك
-app.get('/', handleVerification);
-app.get('/webhook', handleVerification);
+module.exports = app;
